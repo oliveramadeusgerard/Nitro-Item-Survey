dfa9ece0fb0c1781a0fff09b335a33cf68539110
oliveramadeusgerard <oliveracrook@gmail.com>
2026-04-13 16:39:06 -0700

    Add files via upload

---
+++ Season Rewards _ Nitro Type_files/servers.js
@@ -0,0 +1,5 @@
+window.RT_SERVERS_JS = Math.floor(Date.now()/1000);
+window.NTSERVERS = function NTSERVERS(){
+  return ["realtime1.nitrotype.com"];
+}
+localStorage.setItem('rt_servers', '["realtime1.nitrotype.com"]');
